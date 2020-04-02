7dc44f65ba7f242c3ac72e79ada892dd9e6b93eb
galij899 <47144051+galij899@users.noreply.github.com>
2020-04-02 15:46:52 +0300

    исправлена краши при пустом сообщении теперь картавит латинские p и P добавлена команда волк и создан json для генератора цитат

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import vk_api.vk_api
+import json
 import re
+import random
 
 from vk_api.utils import get_random_id
 from vk_api.bot_longpoll import VkBotLongPoll
@@ -23,23 +25,36 @@ class Server:
         self.vk_api = self.vk.get_api()
 
     def test(self):
-        self.vk_api.messages.send(user_id=181178506, message="Привет-привет!", random_id=get_random_id())
+        self.vk_api.messages.send(user_id=181178506, message="Бот запущен", random_id=get_random_id())
 
     def start(self):
         for event in self.long_poll.listen():   # Слушаем сервер
             # Пришло новое сообщение
             if event.type == VkBotEventType.MESSAGE_NEW:
+                # print([event.object['message']['text']])
+
                 if re.findall('откартавь', event.object['message']['text']):
                     res = ''
-                    for i in re.findall("откартавь (.*)", event.object['message']['text'])[0]:
-                        if i == 'р':
-                            res += 'гх'
-                        elif i == 'Рэ':
-                            res += 'Гх'
-                        else:
-                            res += i
-                    print(res)
-                    self.send_message(event.chat_id, res)
+                    message = re.findall("откартавь (.*)", event.object['message']['text'])
+                    if message:
+                        for i in message[0]:
+                            if i == 'р' or i == 'p':
+                                res += 'гх'
+                            elif i == 'Рэ' or i == 'P':
+                                res += 'Гх'
+                            else:
+                                res += i
+                        print(res)
+                        self.send_message(event.chat_id, res)
+                    else:
+                        self.send_message(event.chat_id, 'Нечего тут откартавить')
+
+                elif re.findall('волк', event.object['message']['text']):
+                    with open("wolfs.json", "r") as read_file:
+                        data = json.load(read_file)
+                    i = random.randrange(0, len(data['quotes']), 1)
+                    self.send_message(event.chat_id, data['quotes'][i])
+
                 else:
                     self.send_message(event.chat_id, "Ну, я получил твое сообщение, и что?")
 
